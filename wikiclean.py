5b608bed48a04fdd92b5b42d29bbdd4913dfd458
Matt Satriano <msatriano@gmail.com>
2013-09-22 01:21:25 +0100

    tables seems to work now

--- wikiclean.py
+++ wikiclean.py
@@ -58,10 +58,43 @@ def strip_File_tags( data ):
 			if count == 0:
 				counting = False
 				last_write = i + 2
+			i += 2
+		else:
+			i += 1
+	
+	return final + data[last_write:]
+
+
+
+def strip_tables( data ):
+	count = 0
+	counting = False
+	last_write = 0
+	final = ""
+	i = 0
+	while i < len(data) - 2:
+		if i < len(data) - 19 and count == 0 and data[i:i+19] =='{| class="wikitable':
+			counting = True
+			final += data[last_write : i]
+			count += 1
+			i += 19
+		elif i < len(data) - 18 and count == 0 and data[i:i+18] =='{|class="wikitable':
+			counting = True
+			final += data[last_write : i]
+			count += 1
+			i += 18
+		elif counting and data[i:i+2] == '{|':
+			count += 1
+			i += 2
+		elif counting and data[i:i+2] == '|}':
+			count -= 1
+			if count == 0:
+				counting = False
+				last_write = i + 2
 				i += 2
 		else:
 			i += 1
-
+	
 	return final + data[last_write:]
 
 
@@ -70,7 +103,8 @@ def strip_wikilinks( data ):
 	function1 = lambda x : x.group(1) if not x.group(2) else x.group(2)
 	function2 = lambda x:  x.group(1)
 
-	level0 = strip_File_tags( data )
+	level0 = strip_File_tags( data + ' ')
+	level0 = strip_tables( level0 )
 
 	r = "\[\[[^\[]+?\|([^\|]*?)\]\]"
 	level1 = re.sub(r, function2, level0)
